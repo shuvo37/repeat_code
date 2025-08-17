6760c436d06e34c2728e6ab1e317b9fe2cba937d
shuvo37 <110215540+shuvo37@users.noreply.github.com>
2025-08-17 11:33:14 +0600

    Create ncr.cpp

---
+++ ncr.cpp
@@ -0,0 +1,25 @@
+const int N = 300500;
+const int mod = 1000000007;
+ll fact[N];
+ll invFact[N];
+
+ll fast_pow(ll a, ll p) {
+    ll res = 1;
+    while (p) {
+        if (p % 2 == 0) {
+            a = (a * a) % mod;
+            p /= 2;
+        } else {
+            res = (res * a) % mod;
+            p--;
+        }
+    }
+    return res;
+}
+
+ll C(int n, int k) {
+    if (k > n) {
+        return 0;
+    }
+    return fact[n] * invFact[k] % mod * invFact[n - k] % mod;
+}
